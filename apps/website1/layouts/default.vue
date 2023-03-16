c827dfe19f7d77659cb39f1cc4c3369f6277afcd
Kevin Gorjan <kevingorjan@Kevins-MacBook-Pro.local>
2023-03-16 11:34:45 +0100

    small changes

--- apps/website1/layouts/default.vue
+++ apps/website1/layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        <h1 class="text-xl">Welcome to website 1</h1>
-        <slot />
+  <div>
+    <h1 class="text-xl">Welcome to website 1</h1>
+    <slot />
+    <hr />
+    <div
+      class="relative max-w-xs overflow-hidden bg-cover bg-no-repeat">
+      <img
+        src="https://tecdn.b-cdn.net/img/new/fluid/city/113.webp"
+        class="max-w-xs transition duration-300 ease-in-out hover:scale-105"
+        alt="Louvre" />
     </div>
+  </div>
 </template>
--- apps/website1/pages/index.vue
+++ apps/website1/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <KeepAlive exclude="['Button']">
-    <sui-button :click="handleOnClick"> 
+    <sui-button :click="handleOnClick" :btnclass="'bg-red-500'"> 
       <span class="relative">{{ $hello('website 1') }}</span>
     </sui-button>
   </KeepAlive>
